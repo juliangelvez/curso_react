6478e732a5cfa3b8e4942c6ae1e241243b316483
Julian Gelvez <julianleonardogelvezpardo@gmail.com>
2023-02-28 10:51:14 -0500

    Primera parte curso react jab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import imagen0 from './img/rey_atanagildo.png';
+import imagen1 from './img/rey_leogivildo.png';
+import imagen2 from './img/rey_sisebuto.png';
 import './App.css';
+import './miCss.css';
+
 
 function App() {
+  let nombres = ["Atanagildo","Leovigildo","Sistebuto"]
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      <div className='caja'>
+        <img src={imagen0} alt="" />
+        <div className='nombre'> {nombres[0]} </div>
+      </div>
+      <div className='caja'>
+        <img src={imagen1} alt="" />
+        <div className='nombre'> {nombres[1]} </div>
+      </div>
+      <div className='caja'>
+        <img src={imagen2} alt="" />
+        <div className='nombre'> {nombres[2]} </div>
+      </div>
     </div>
   );
 }
